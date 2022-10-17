b78051285f447a882753b14396cd4e931d397e23
farjana74 <csefarjana74@gmail.com>
2022-10-17 15:28:12 +0600

    added search component

---
+++ src/pages/searchBar/index.tsx
@@ -0,0 +1,35 @@
+import SearchIcon from '@mui/icons-material/Search'
+
+const SearchBar = ({ placeholder }: any) => {
+  return (
+    <>
+      <div className='search'>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            border: '2px solid black',
+            width: '40%',
+            padding: '8px',
+            background: '#FFFFFF',
+            boxShadow: '2px 5px 20px rgba(22, 31, 41, 0.1)',
+            borderRadius: '20px'
+          }}
+          className='searchInput'
+        >
+          <input style={{ border: 0, outline: 'none' }} type='text' placeholder={placeholder} />
+          <div className='searchIcon'>
+            <SearchIcon />
+          </div>
+        </div>
+      </div>
+
+      {/* <div>
+        <h2>{value}</h2>
+      </div> */}
+    </>
+  )
+}
+
+export default SearchBar
