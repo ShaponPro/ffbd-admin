4916517b04a10d31458a8ba26a22886ffddbdb46
Mokarram77117 <mokarram.ice.ru@gmail.com>
2022-10-18 17:22:01 +0600

    component/button:Simple Button Component

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,21 +1,51 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
 type props = {
-    title: string;
-    type: string;
-    onClick: (args: any) => void;
-    style: React.CSSProperties;
+  children: string
+  type: string
+  styles?: React.CSSProperties
 }
 export const ButtonComponent = (props: props) => {
-    console.log('props :>> ', props);
-  return (
-    <div>ButtonComponent</div>
-  )
+  let style = props.styles
+  console.log('props :>> ', props)
+  if (props.type === 'navButton')
+    return (
+      <div>
+        <button
+          style={{
+            width: '200px',
+            height: '40px',
+            background: ' #FFFFFF80',
+            padding: '10px 20px',
+            borderRadius: '20px',
+            fontSize: '16px',
+            fontWeight: '400',
+            boxShadow: 'inset 1px 1.5px 5px rgba(22, 31, 41, 0.1)'
+          }}
+        >
+          {props.children}
+        </button>
+      </div>
+    )
+  else if (props.type === 'actionButton')
+    return (
+      <div>
+        <button
+          style={{
+            width: '200px',
+            height: '40px',
+            background: '##161F29',
+            padding: '10px 20px',
+
+            fontSize: '16px',
+            fontWeight: '400',
+            boxShadow: 'inset 1px 1.5px 5px rgba(22, 31, 41, 0.1)'
+          }}
+        >
+          {props.children}
+        </button>
+      </div>
+    )
 }
 
-const mapStateToProps = (state: any) => ({})
-
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ButtonComponent)
+export default ButtonComponent
