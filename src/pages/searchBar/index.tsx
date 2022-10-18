12805dd7d0660ccb2653ce2da641ee0a5eb37316
farjana74 <csefarjana74@gmail.com>
2022-10-18 10:57:04 +0600

    added search

--- src/pages/searchBar/index.tsx
+++ src/pages/searchBar/index.tsx
@@ -14,7 +14,8 @@ const SearchBar = ({ placeholder }: any) => {
             padding: '8px',
             background: '#FFFFFF',
             boxShadow: '2px 5px 20px rgba(22, 31, 41, 0.1)',
-            borderRadius: '20px'
+            borderRadius: '20px',
+            position: 'absolute'
           }}
           className='searchInput'
         >
