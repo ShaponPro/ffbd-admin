f81ed56336f0238d24c6682db5167e1899df048c
coder-ocd <seuianbinte@gmail.com>
2022-10-28 17:40:06 +0600

    Add data grid

--- src/pages/components/list.tsx
+++ src/pages/components/list.tsx
@@ -1,15 +1,15 @@
-import ListComponent from 'src/components/ListComponent'
-import { Grid } from '@mui/material'
+import ListComponent from "src/components/ListComponent";
+import { Grid } from "@mui/material";
 
 const index = () => {
-  return (
-    <div>
-      <Grid container spacing={6}>
-        <Grid item xs={12}>
-          <ListComponent />
-        </Grid>
-      </Grid>
-    </div>
-  )
-}
-export default index
+    return (
+        <div>
+            <Grid container spacing={6}>
+                <Grid item xs={12}>
+                    <ListComponent />
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+export default index;
---
+++ src/views/videos/upload-a-video/drafts/drafts-components/DraftComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ListComponent from "./ListComponentCopy";
+
+const DraftComponent = () => {
+    return (
+        <>
+            <ListComponent></ListComponent>
+        </>
+    );
+};
+
+export default DraftComponent;
