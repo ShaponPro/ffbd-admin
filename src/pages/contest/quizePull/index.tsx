0e9cf7dc2b9b3fbe8cc01aa753cb25d70917d2d2
farjana74 <csefarjana74@gmail.com>
2022-11-02 11:05:56 +0600

    added quize pull background

---
+++ src/pages/contest/quizePull/index.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+
+const index = () => {
+    return (
+        <Box
+            style={{
+                background: "#FFFFFF",
+                boxShadow: "2px 5px 20px rgba(22, 31, 41, 0.1)",
+                borderRadius: "10px",
+                height: "236px",
+                padding: "20px",
+            }}
+        >
+            <h3>fkghhdfikug</h3>
+        </Box>
+    );
+};
+
+export default index;
