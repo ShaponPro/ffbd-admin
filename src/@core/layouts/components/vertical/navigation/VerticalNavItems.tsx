9d57e3d28f2dee455b15070c8ca0d7f231a23919
farjana74 <csefarjana74@gmail.com>
2022-11-02 10:50:24 +0600

    adeed dynamic logic of sidebar

--- src/@core/layouts/components/vertical/navigation/VerticalNavItems.tsx
+++ src/@core/layouts/components/vertical/navigation/VerticalNavItems.tsx
@@ -1,50 +1,56 @@
 // ** Types Import
-import { Settings } from 'src/@core/context/settingsContext'
+import { Settings } from "src/@core/context/settingsContext";
 import {
-  NavGroup,
-  NavLink,
-  NavSectionTitle,
-  VerticalNavItemsType
-} from 'src/@core/layouts/types'
+    NavGroup,
+    NavLink,
+    NavSectionTitle,
+    VerticalNavItemsType,
+} from "src/@core/layouts/types";
 
 // ** Custom Menu Components
-import VerticalNavGroup from './VerticalNavGroup'
-import VerticalNavLink from './VerticalNavLink'
-import VerticalNavSectionTitle from './VerticalNavSectionTitle'
+import VerticalNavGroup from "./VerticalNavGroup";
+import VerticalNavLink from "./VerticalNavLink";
+import VerticalNavSectionTitle from "./VerticalNavSectionTitle";
 
 interface Props {
-  parent?: NavGroup
-  navHover?: boolean
-  settings: Settings
-  navVisible?: boolean
-  groupActive: string[]
-  isSubToSub?: NavGroup
-  currentActiveGroup: string[]
-  navigationBorderWidth: number
-  verticalNavItems?: VerticalNavItemsType
-  saveSettings: (values: Settings) => void
-  setGroupActive: (value: string[]) => void
-  setCurrentActiveGroup: (item: string[]) => void
+    parent?: NavGroup;
+    navHover?: boolean;
+    settings: Settings;
+    navVisible?: boolean;
+    groupActive: string[];
+    isSubToSub?: NavGroup;
+    currentActiveGroup: string[];
+    navigationBorderWidth: number;
+    verticalNavItems?: VerticalNavItemsType;
+    saveSettings: (values: Settings) => void;
+    setGroupActive: (value: string[]) => void;
+    setCurrentActiveGroup: (item: string[]) => void;
 }
 
-const resolveNavItemComponent = (item: NavGroup | NavLink | NavSectionTitle) => {
-  if ((item as NavSectionTitle).sectionTitle) return VerticalNavSectionTitle
-  if ((item as NavGroup).children) return VerticalNavGroup
+const resolveNavItemComponent = (
+    item: NavGroup | NavLink | NavSectionTitle
+) => {
+    if ((item as NavSectionTitle).sectionTitle) return VerticalNavSectionTitle;
+    if ((item as NavGroup).children) return VerticalNavGroup;
 
-  return VerticalNavLink
-}
+    return VerticalNavLink;
+};
 
 const VerticalNavItems = (props: Props) => {
-  // ** Props
-  const { verticalNavItems } = props
+    // ** Props
+    const { verticalNavItems } = props;
 
-  const RenderMenuItems = verticalNavItems?.map((item: NavGroup | NavLink | NavSectionTitle, index: number) => {
-    const TagName: any = resolveNavItemComponent(item)
+    // console.log(verticalNavItems);
 
-    return <TagName {...props} key={index} item={item} />
-  })
+    const RenderMenuItems = verticalNavItems?.map(
+        (item: NavGroup | NavLink | NavSectionTitle, index: number) => {
+            const TagName: any = resolveNavItemComponent(item);
 
-  return <>{RenderMenuItems}</>
-}
+            return <TagName {...props} key={index} item={item} />;
+        }
+    );
 
-export default VerticalNavItems
+    return <>{RenderMenuItems}</>;
+};
+
+export default VerticalNavItems;
