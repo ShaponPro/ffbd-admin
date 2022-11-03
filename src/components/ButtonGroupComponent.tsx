8a2a0a2870e452a39ff2c72d7e74eee0b82d8e31
afsanaeva <afsanaeva05@gmail.com>
2022-11-03 14:06:53 +0600

    button group desgine done

--- src/components/ButtonGroupComponent.tsx
+++ src/components/ButtonGroupComponent.tsx
@@ -1,52 +1,76 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+// ** React Imports
+import { Fragment } from "react";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#FFFFFF",
-  padding: "10px",
-  textAlign: 'center',
-  color: "#161F29",
-  fontWeight: "400",
-  fontSize: "14px",
+// ** MUI Imports
+import Button, { ButtonProps } from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+// Styled component for a custom button
+const CustomButton = styled(Button)<ButtonProps>(({ theme }) => ({
+    backgroundColor: "#FFFFFF",
+    color: "black",
+    fontWeight: "400",
+    "&:hover": {
+        backgroundColor: " #EBE8FC",
+        color: "black",
+        fontWeight: "700",
+    },
+    width: "242px",
+    padding: "10px",
+    fontSize: "14px",
+    borderRadius: "5px",
+    textTransform: "capitalize",
 }));
 
-function FormRow() {
-  return (
-    <React.Fragment>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-      
-    </React.Fragment>
-  );
-}
+const ButtonGroupComponent = () => {
+    return (
+        <Fragment>
+            <div className="demo-space-x">
+                <CustomButton variant="contained" size="large">
+                    Contest Info
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                    Rewards
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                    Performance
+                </CustomButton>
+            </div>
+            <div className="demo-space-x">
+                <CustomButton variant="contained" size="large">
+                    Participants
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                    Videos
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                Potential List
+                </CustomButton>
+            </div>
+            <div className="demo-space-x">
+                <CustomButton variant="contained" size="large">
+                Voted Ranking
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                Irrelevant List
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                Shortlist
+                </CustomButton>
+            </div>
+            <div className="demo-space-x">
+                <CustomButton variant="contained" size="large">
+                Pre-Result/Final Wheel
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                Result
+                </CustomButton>
+                <CustomButton variant="contained" size="large">
+                Disburse Gift
+                </CustomButton>
+            </div>
+        </Fragment>
+    );
+};
 
-export default function ButtonGroupComponent() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1}>
-        <Grid container item spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item spacing={3}>
-          <FormRow />
-        </Grid>
-      </Grid>
-    </Box>
-  );
-}
+export default ButtonGroupComponent;
--- src/pages/components/buttonGroup.tsx
+++ src/pages/components/buttonGroup.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ButtonGroupComponent from 'src/components/ButtonGroupComponent';
 
+// import { styled } from '@mui/material/styles';
+// const Item = styled(Paper)(({ theme }) => ({
+//     backgroundColor: "#FFFFFF",
+//     padding: "10px",
+//     textAlign: 'center',
+//     color: "#161F29",
+//     fontWeight: "400",
+//     fontSize: "14px",
+//   }));
 const buttonGroup = () => {
     return (
         <div>
