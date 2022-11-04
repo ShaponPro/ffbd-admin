ea6ecaac48b28f4defa31fc1424beca833ca55ce
afsanaeva <afsanaeva05@gmail.com>
2022-11-04 16:08:43 +0600

    list table component done

--- src/components/ListTableComponent.tsx
+++ src/components/ListTableComponent.tsx
@@ -2,8 +2,8 @@
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableRow from '@mui/material/TableRow';
 import styled from "@emotion/styled";
 
@@ -21,6 +21,23 @@ const rows = [
   createData('Contest Name',  ' - '),
   createData('Cover Image',  ' - '),
   createData('Short Description',  ' - '),
+  createData('Description',  ' - '),
+  createData('Hashtag',  ' - '),
+  createData('Brand Tag',  ' - '),
+  createData('Participation Tag',  ' - '),
+  createData('Target User Level',  ' - '),
+  createData('Target Age Group',  ' - '),
+  createData('Target Gender',  ' - '),
+  createData('Target Location',  ' - '),
+  createData('Target Country',  ' - '),
+  createData('Target Tags',  ' - '),
+  createData('Start Date',  ' - '),
+  createData('End Date',  ' - '),
+  createData('Duration Days',  ' - '),
+  createData('Publishing Brand Name',  ' - '),
+  createData('Publishing Brand ID',  ' - '),
+  createData('Uploaded By',  ' - '),
+  createData('Approved By',  ' - '),
 ];
 
 //**Style */
@@ -31,22 +48,32 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     "&:nth-of-type(odd)": {
         backgroundColor: "rgba(22, 31, 41, 0.07);",
     },
+    border:"2px solid rgba(22, 31, 41, 0.03)"
+}));
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: "12px",
+        fontWeight: 700,
+        border: "2px solid white",
+        width: "50%",
+    },
 }));
 
 export default function ListTableComponent() {
   return (
-    <TableContainer >
-      <Table sx={{ minWidth: 300 }} aria-label="simple table">
+    <TableContainer sx={{backgroundColor: "#EBF9ED"}}>
+      <Table sx={{ minWidth: 400 }} aria-label="simple table">
         <TableBody>
           {rows.map((row) => (
             <StyledTableRow
               key={row.name}
               sx={{ ' &:last-child th': { border: 0 } }}
             >
-              <TableCell sx={{fontWeight:700}} component="th" scope="row">
+              <StyledTableCell sx={{fontWeight:700}} component="th" scope="row">
                 {row.name}
-              </TableCell>
-              <TableCell >{row.tableValue}</TableCell>
+              </StyledTableCell>
+              <StyledTableCell align="left">{row.tableValue}</StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
