77627a6061a82330e808711f09806c2ae8ad75e8
afsanaeva <afsanaeva05@gmail.com>
2022-11-08 13:53:30 +0600

    contest result page winner tab componant create and tab changes

---
+++ src/pages/contest/contest-result/winnersTab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContestWinnersTabComponent from "../../../views/contest/ContestWinnersTabComponent"
+
+const DemoPages = () => {
+  return (
+    <>
+      <ContestWinnersTabComponent/>
+    </>
+  );
+};
+
+export default DemoPages;
---
+++ src/views/contest/ContestScoreSettingTabComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContestScoreSettingTabComponent = () => {
+  return (
+    <>
+     Setting-Video Score under a Contest
+    </>
+  );
+};
+
+export default ContestScoreSettingTabComponent;
